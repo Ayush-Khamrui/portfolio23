a89f7dc4c21b66c41a5ad62b002e713e134c4968
Ayush-Khamrui <ayushkhamrui@outlook.com>
2023-02-06 01:51:07 +0530

    about section added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,18 +3,12 @@ import React from 'react'
 export default function Navbar() {
     const navMenu = {
         color: '#FFFFFF',
-        fontSize: '120%'
+        fontSize: '120%',
+        marginLeft: '60%'
 
     };
-
-    const navLogo = {
-        marginLeft: '-200%',
-        fontSize: '120%',
-        fontWeight: 'bold',
-        textShadow: '2px 2px red'
-    }
     return (
-            <div className="container-fluid" >
+            <div className="Navbar container" >
                 <header className="d-flex justify-content-center py-3">
                     <ul className="nav nav-pills" style={navMenu}>
                         <li className="nav-item"><a href="/" className="nav-link text-light" aria-current="page">About</a></li>
